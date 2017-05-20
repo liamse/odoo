4f3454c003ae840e40bbaab408914a3056b91412
esmaeil <esmaeil.samadi@nak-mci.ir>
2017-05-20 16:04:07 +0430

    960230

--- web_search_addons/README.md
+++ web_search_addons/README.md
@@ -1,6 +1,14 @@
-# List of Modules
+#Comma as OR Search Addons
 
-- Chatter Date Fixer
-	- A major problem in Odoo 7.0 is dates in "Chatter" part. This date after one week change to a long number that does not means to end user. After install this module dates shows as well as it expected.
+One major use of search is related to search a list of items. Most of the time this items are listed in Excel or CSV file.
+This module help to copy a list of comma seperated items into search box and search all of them in once. In essence, the comma here is like OR operator.
+## How to use copy from Excel
+![Excel screen on how to copy a list with comma](/images/excel_search.png?raw=true "Excel screen on how to copy a list with comma")
+To make it simpler, in Excel, follow this scenario.
+First of all, create a column of your items that want to search, next add a column in right of it with commas, Finally copy two columns in once and paste it into search box.
+## How to use directly
+Wrtie directly into search box or Advance Search box with comma separation or paste it from Excel.
+![Write directly or paste it](/images/paste_in_advance_search.png?raw=true "Excel screen Write directly or paste it")
+
 
 
--- web_search_addons/__openerp__.py
+++ web_search_addons/__openerp__.py
@@ -6,13 +6,13 @@
     'website' : 'www.webirani.com',
     'summary' : 'Comma as OR Search Addons',
     "description": """
-    Comma as OR Search Addons
-    =========================
-    One major use of search is related to search a list of items. Most of the time this items are listed in Excel or CSV file.
-    This module help to copy a list of comma seperated items into search box and search all of them in once. In essence, the comma here is like OR operator.
-    To make it simpler, in Excel, follow this scenario.
-    First of all, create a column of your items that want to search, next add a column in right of it with commas, Finally copy two columns in once and paste it into search box.
-    This feature works in Advance Search as well.
+Comma as OR Search Addons
+=========================
+One major use of search is related to search a list of items. Most of the time this items are listed in Excel or CSV file.
+This module help to copy a list of comma seperated items into search box and search all of them in once. In essence, the comma here is like OR operator.
+To make it simpler, in Excel, follow this scenario.
+First of all, create a column of your items that want to search, next add a column in right of it with commas, Finally copy two columns in once and paste it into search box.
+This feature works in Advance Search as well.
 
 """,
     "depends": [
